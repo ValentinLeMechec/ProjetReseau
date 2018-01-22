84c7199ea48ab05449f18641a2f9c124cbb9dbba
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2018-01-22 18:22:14 +0100

    esclave

--- Esclave.java
+++ Esclave.java
@@ -1,9 +1,35 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class Esclave {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		try {
+			Socket socket = new Socket(InetAddress.getByName("172.18.50.20"), 56565);
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 	}
 
 }
