94a416d03b3304df2fd71473e4dd3fc5e3719348
Unknown <dylan.renard.72@gmail.com>
2018-01-30 16:21:43 +0100

    maitre

--- Maitre.java
+++ Maitre.java
@@ -1,21 +1,18 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
-public class Maitre
+public class Maitre extends Esclave
 {
 	static int compteur = 1;
-	
-
 	
 	public static void main(String[] args) 
 	{
@@ -23,32 +20,43 @@ public class Maitre
 		
 		try
 		{
-			Socket socket = new Socket(InetAddress.getByName("172.18.50.99"), 56565);
-			ObjectOutputStream buffOut= new ObjectOutputStream(socket.getOutputStream());
-			ObjectInputStream buffIn= new ObjectInputStream(socket.getInputStream());
+			Socket socket = new Socket(InetAddress.getByName("172.18.50.20"), 56565);
+			OutputStream buffOut= socket.getOutputStream();
+			InputStream buffIn= socket.getInputStream();
 			
 			/*envoi au serveur du choix du mode*/
 			System.out.println("Veuillez entrer le mode:");
 			BufferedReader consoleIn= new BufferedReader(new InputStreamReader(System.in));
 			String mode= consoleIn.readLine();
 			
-			buffOut.writeObject(mode);
+			buffOut.write(mode.getBytes());
 			buffOut.flush();
 			
-			System.out.println("Veuillez entrer le chemin:");
-			String chemin= consoleIn.readLine();
+			/*if(args.length>=1)  
+			{
+				if(args.length==2) 
+				{
+					file = new File(args[1] + "\\racine");
+					file.mkdirs();
+				}
+				else 
+				{
+					file = new File("H:\\Mes documents\\ProgReseauProjet\\racine"); 
+					file.mkdirs();
+
+				}
+			}
+			else
+			{
+				file = new File("H:\\Mes documents\\ProgReseauProjet\\racine");
+				file.mkdirs();
+
+			}*/
 			
-			
-			
-			racine = new File(chemin);
+			racine = new File("H:\\Mes documents\\ProgReseauProjet\\racine");
 			racine.mkdirs();
 			
-			try {
-				envoi(racine, buffOut, buffIn);
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			envoi(racine, buffOut, buffIn);
 			
 		}
 		catch (IOException e) 
@@ -59,15 +67,14 @@ public class Maitre
 
 	
 	
-	public static void envoi(File f, ObjectOutputStream out, ObjectInputStream in) throws IOException, ClassNotFoundException 
+	public static void envoi(File f, OutputStream out, InputStream in) throws IOException 
 	{
 		int compteurcourrant = compteur++;
 		
 		String message;
-		String data = new String();
+		byte[] data = new byte[1024];
 		BufferedReader br;
 
-	
 		File[] list = f.listFiles();
 		
 		if(list.length>0) 
@@ -76,47 +83,58 @@ public class Maitre
 			{
 				if(list[i].isDirectory())
 				{
-					out.writeObject(list[i].getAbsolutePath() + "  dir  " + list[i].lastModified());
-					
+					message = list[i].getAbsolutePath() + "  dir  " + list[i].lastModified();
+					out.write(message.getBytes());
+					out.flush();
 					
 					envoi(list[i], out,in);
 				}
 				else 
 				{
+					br= new BufferedReader(new FileReader(list[i]));
 					
-					out.writeObject(list[i].getAbsolutePath() + "  file  " + list[i].lastModified());
+					message = list[i].getAbsolutePath() + "  file  " + list[i].lastModified();
+					out.write(message.getBytes());
+					out.flush();
 					
-					
-					message = in.readObject().toString();
+					if(in.available()!=0)in.read(data);
+					message = "";
+					for (byte b : data)
+					{
+						message += (char)b;
+					}
+					System.out.println(message);
 					
 					if(message.equals("PASOK"))
 					{	
-						BufferedReader fis = new BufferedReader(new FileReader(list[i]));
-						
-						while((data=fis.readLine())!=null)
+						while((message=br.readLine())!=null) 
 						{
-							out.writeObject(data);
+							out.write(message.getBytes());
+							out.flush();
 						}
 						
-						data = "null";
-						out.writeObject(data);
-						fis.close();
+						message = "null";
+						out.write(message.getBytes());
+						out.flush();
 					}
+					br.close();
 				}			
 			}
 		}
-		
 		System.out.println("fin de l'envoi " + compteurcourrant);
 		if(compteurcourrant!=1) 
 		{
-			out.writeObject("null");
-			
+			message = "null";
+			out.write(message.getBytes());
+			out.flush();
 			
 		}
 		else 
 		{
-			System.out.println("\n\n\nfinRacine "+compteurcourrant);
-			out.writeObject("finRacine"+compteurcourrant);
+			System.out.println("finRacine"+compteurcourrant);
+			message = "finRacine"+compteurcourrant;
+			out.write(message.getBytes());
+			out.flush();
 			in.close();
 			out.close();
 		}
